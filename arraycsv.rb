c409a638912739849a667d1e5b0a58ddafcd3993
nonnax <ironald@gmail.com>
2021-09-10 19:30:36 +0800

    initial commit

---
+++ arraycsv.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+# Id$ nonnax 2021-09-10 17:39:07 +0800
+# csv pseudo-array 
+require 'csv'
+require 'forwardable'
+
+class ArrayCSV
+  extend Forwardable
+  attr_accessor :dataframe
+
+  def_delegators :@dataframe, :[], :values_at, :size, :first, :last, :empty?, :map, :each, :sort_by
+  
+  def initialize(fname)
+    @fname=fname
+    # @dataframe=[]
+    load
+  end  
+  def <<(data)
+      CSV.open(@fname, 'a' ){|row| row<<data }
+      load
+  end
+  alias append <<
+
+  private
+  
+  def load
+    @dataframe=File.exists?(@fname) ? CSV.parse(File.read(@fname), converters: %i[numeric]) : []
+  end
+  alias load_data load
+
+end
+
+__END__
+
+data=ArrayCSV.new('test.csv')
+10.times do
+  data<<[Time.now.to_s, rand(10..100), rand(90..100), rand(10..90), rand(10..100)]
+end
+
+p data.class
+p data.methods-Object.methods
+p data.size
+p data.values_at(-1, 1)
